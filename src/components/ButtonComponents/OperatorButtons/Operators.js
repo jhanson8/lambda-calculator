0088e30f19ea1eb50c571c80d7295ce4775445d0
Jonathan Hanson <jhanson8@uoregon.edu>
2019-12-10 16:32:48 -0800

    first commit

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,4 +1,7 @@
-import React from "react";
+/* jshint esversion: 6 */
+import React, {useState} from "react";
+import { operators } from "../../../data.js";
+import OperatorButton from "./OperatorButton.js";
 
 //import any components needed
 
@@ -6,11 +9,17 @@ import React from "react";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [operatorButton, setOperatorButton] = useState(operators);
+  console.log(operators);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
-    </div>
+       it any props needed by the child component*/
+       operatorButton.map((button, index) => (<OperatorButton key={index} button={button} />))}
+     </div>
+
   );
 };
+
+export default Operators;
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,16 @@
+/* jshint esversion: 6 */
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button className="special_button">
+        {/* since props is an object we use the . notation */}
+        {props.button}
+      </button>
     </>
   );
 };
+
+export default SpecialButton;
